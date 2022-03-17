ec35dfd390c338530d7ac8a26a1ec84402465ece
Filip <sieniu212@gmail.com>
2022-03-17 18:19:05 +0100

    1

---
+++ List-2/1.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int *wypelnij()
+{
+    static int a[10];
+
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++)
+    {
+        a[i] = rand() % 100 + 1;
+    }
+
+    return a;
+}
+
+int odczytaj(int a[])
+{
+    int *adres = a;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << *(adres + i) << " ";
+    }
+    return 0;
+}
+
+int main()
+{
+
+    int *a = wypelnij();
+
+    odczytaj(a);
+
+    return 0;
+}
---
+++ List-3/1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct lista
+{
+    string imie;
+    string nazwisko;
+    string PESEL;
+    int wiek;
+    lista *pop;
+};
+
+int main()
+{
+
+    return 0;
+}
