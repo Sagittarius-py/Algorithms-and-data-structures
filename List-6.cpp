8ca79fd030cd9783caa85eff76b96c195aceae02
Filip Sieniawski <71313520+Sagittarius-py@users.noreply.github.com>
2022-04-22 08:58:56 +0200

    Create List-6.cpp
    1

---
+++ List-6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct lista
+{ string imie;
+    string nazwisko;
+    string PESEL;
+    int wiek;
+    lista *pop;
+    lista *nast;
+};
+
+void dodaj(){
+    lista *tablica = new lista();
+
+};
+
+int main(){
+
+
+
+    return 0;
+};
